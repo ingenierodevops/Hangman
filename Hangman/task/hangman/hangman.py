4b227f3e6e121df425d90c924abc57e8456d494a
ingenierodevops <77163397+ingenierodevops@users.noreply.github.com>
2021-01-18 12:12:50 +0100

    Random words

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,7 +1,17 @@
+from random import choice
+
 # Write your code here
-print("""H A N G M A N
-Guess the word: """)
-word = "python"
+word_list = ('python', 'java', 'kotlin', 'javascript')
+
+welcome_text= """H A N G M A N
+Guess the word: """
+
+print(welcome_text)
+
+
 user_word = input()
+
+#word = "python"
+word = choice(word_list)
 
 print("You survived!" if user_word == word else "You lost!")
